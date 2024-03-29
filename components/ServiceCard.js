ebefd4839504491694345e89e45eb0b74894ab8a
Arijeet10 <arijeetsocial@gmail.com>
2024-03-29 07:51:41 +0530

    chaintech assignment landing page

--- components/ServiceCard.js
+++ components/ServiceCard.js
@@ -4,7 +4,7 @@ const ServiceCard = ({cardData}) => {
     return ( 
         <>
             <div className={`md:overflow-hidden flex flex-col border-2 border-black rounded-lg ${cardData.color=="green" && "bg-green-100"} ${cardData.color=="cyan" && "bg-cyan-100"} ${cardData.color=="pink" && "bg-pink-100"} ${cardData.color=="purple" && "bg-purple-100"}`}>
-                <div className="h-[15%] flex items-center justify-start gap-1 pr-36 pl-4 py-6 md:pr-4 ">
+                <div className="h-[15%] flex items-center justify-start gap-2 pr-36 pl-4 py-4 md:pr-4 ">
                     <div className="bg-black flex items-center justify-center rounded-full p-2">
                         <Image 
                             src="/C.png"
